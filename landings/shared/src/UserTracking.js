34b398f1491c656b73cb8984f75cf5ec94538c10
Vladyslav Slepkan <vladyslav.slepkan@intellectsoft.net>
2016-12-26 10:59:58 +0200

    Removed unused code.

--- landings/shared/src/UserTracking.js
+++ landings/shared/src/UserTracking.js
@@ -19,8 +19,6 @@ const DATE = Date.now();
 const LOCATION = document.location;
 const REFERRER = document.referrer;
 
-const GA_COOKIE = Cookies.get('_ga').slice(6); // Get clean CID.
-
 const handleCookiesOptions = options => (name, func) => Cookies.set(name, func, options); // Curry
 const addCookie = handleCookiesOptions(cookieOptions);
 
