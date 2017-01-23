3d7d166f760aeca6ad4a465b627f5bee0f1bdc3a
Vladyslav Slepkan <vladyslav.slepkan@intellectsoft.net>
2017-01-23 13:58:58 +0200

    UCS-1857: Changed semicolon for medium_history to comma.

--- landings/shared/src/UserTracking.js
+++ landings/shared/src/UserTracking.js
@@ -67,7 +67,7 @@ function setMediumHistory () {
     const mediumCookie = Cookies.get('is_medium_history');
     const userDate = Cookies.get('is_timer');
 
-    const createMediumBuffer = mediumCookie && mediumCookie.split(';') || [];
+    const createMediumBuffer = mediumCookie && mediumCookie.split(',') || [];
     const currentDate = Date.now();
     const isTimerDone = (Number(userDate) + DELAY) - currentDate;
     const lastElementOfArray = createMediumBuffer[createMediumBuffer.length - 1];
@@ -76,8 +76,8 @@ function setMediumHistory () {
         Cookies.set('is_timer', DATE, cookieOptions); // Clear the timer
 
         return getMediumCookie === 'direct'
-            ? createMediumBuffer.push(['direct']) && createMediumBuffer.join(';')
-            : createMediumBuffer.push(getMediumCookie) && createMediumBuffer.join(';')
+            ? createMediumBuffer.push(['direct']) && createMediumBuffer.join(',')
+            : createMediumBuffer.push(getMediumCookie) && createMediumBuffer.join(',')
     } else {
         if (mediumCookie) { // If we already know the user
             return mediumCookie
