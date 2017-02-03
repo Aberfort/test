5e55693922ae6d411dcb03a7806b30396a7cc28b
Vladyslav Slepkan <vladyslav.slepkan@intellectsoft.net>
2017-02-03 16:08:29 +0200

    UCS-1881: Renamed userTracking file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 gulp.task('userTracking', () => {
     return gulp.src('./landings/shared/src/UserTracking.js')
@@ -8,6 +9,10 @@ gulp.task('userTracking', () => {
         presets: ['es2015', 'stage-0']
     }))
     .pipe(uglify())
+    .pipe(rename({
+        basename: 'main',
+        suffix: '.min'
+    }))
     .pipe(gulp.dest('./landings/shared'));
 });
 
