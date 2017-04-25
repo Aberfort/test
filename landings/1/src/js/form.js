bd77c7749d91940d0c507ac9178fa9ec08804cb8
Vladyslav Slepkan <vladyslav.slepkan@intellectsoft.net>
2017-04-25 18:17:57 +0300

    UCS-2128: Added file validation.

--- landings/1/src/js/form.js
+++ landings/1/src/js/form.js
@@ -1,20 +1,23 @@
 const handleFormSubmit = window.handleFormSubmit;
 const mainForm = document.querySelector('#mainForm');
 const getNDACheckbox = document.querySelector('#terms');
-const newAction = mainForm.getAttribute('action');
+const formAction = mainForm.getAttribute('data-url');
 const thxMessage = document.querySelector('.contact__complete');
 
 getNDACheckbox.addEventListener('click', function (e) {
-    e.target.value === '1'
-        ? e.target.value = '0'
-        : e.target.value = '1'
+    e.target.value === '1' ? (e.target.value = '0') : (e.target.value = '1');
 });
 
 $.validator.methods.number = function (value, element) {
     return this.optional(element) || /^[0-9+\-() —]+$/.test(value);
 };
 
-$("#mainForm").validate({
+$.validator.addMethod('filesize', function (value, element, param) {
+    // param = size (in bytes)
+    return this.optional(element) || element.files[0].size <= param;
+});
+
+$('#mainForm').validate({
     rules: {
         first_name: {
             required: true,
@@ -45,21 +48,30 @@ $("#mainForm").validate({
             required: true,
             maxlength: 65535
         },
+        attach: {
+            extension: 'doc|docx|pdf|odt|ott|txt',
+            filesize: 25 * 1000 * 1000
+        }
     },
     messages: {
         email: {
-            email: "Please enter a valid email address."
+            email: 'Please enter a valid email address.'
+        },
+        attach: {
+            filesize: 'File should be less than 25mb'
         }
     },
     submitHandler: function () {
         const rowData = new FormData(mainForm);
         rowData.append('handler_id', mainForm.dataset.handler);
 
-        handleFormSubmit(newAction, rowData, {
+        handleFormSubmit(formAction, rowData, {
             type: mainForm.dataset.type
-        }).then(res => {
-            mainForm.style.display = 'none';
-            thxMessage.style.display = 'block';
-        });
+        })
+            .then(res => {
+                mainForm.style.display = 'none';
+                thxMessage.style.display = 'block';
+            })
+            .catch(error => console.log(error));
     }
 });
