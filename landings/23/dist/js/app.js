f6177a9784f9291eaf3d3c5116ccee1d941c3972
Artur Ivashchenko <a.ivashchenko@spd-ukraine.com>
2019-04-17 17:52:23 +0300

    build 21 landing

---
+++ landings/23/dist/js/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var contactForms = document.body.querySelectorAll('.software-engineering .contact-form');
+
+Array.from(contactForms).forEach(function (form) {
+    var thisForm = form.querySelector('form');
+
+    thisForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        var successMessage = form.querySelector('.success');
+        var data = new FormData(e.target);
+        var url = e.target.dataset.url;
+
+
+        window.handleFormSubmit(url, data, {
+            type: 'ContactForm'
+        }).then(function (response) {
+            if (response.data.status) {
+                thisForm.classList.add('hidden');
+                successMessage.classList.remove('hidden');
+            }
+        }).catch(function (error) {
+            console.error(error);
+        });
+    });
+});
