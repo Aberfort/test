6b87b3684fb7c720d2b03e761ecfdcce0c816d5f
Artur Ivashchenko <a.ivashchenko@spd-ukraine.com>
2019-05-25 17:31:36 +0300

    Added carousel

--- landings/25/source/js/custom/main.js
+++ landings/25/source/js/custom/main.js
@@ -33,3 +33,45 @@ Array.from(contactForms).forEach(form => {
         })
     });
 });
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    dots: false,
+    responsive:{
+        0:{
+            items: 1,
+            nav: false,
+            stagePadding: 40,
+        },
+        500:{
+            nav: false,
+            items: 2,
+            stagePadding: 40,
+        },
+        576:{
+            nav: false,
+            items: 2
+        },
+        620:{
+            nav: true,
+            items: 2
+        },
+        768:{
+            nav: false,
+            items: 3
+        },
+        800:{
+            nav: true,
+            items: 3
+        },
+        992:{
+            nav: false,
+            items: 4
+        },
+        1040:{
+            nav: true,
+            items: 4
+        }
+    }
+});
