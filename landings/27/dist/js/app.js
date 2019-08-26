99e1b1df8a6285a86c522112f9022cef84d7d4a1
Serhii Vasyliev <serhii.vasyliev@root8s-MacBook-Air.local>
2019-08-26 13:45:43 +0300

    Create animations

--- landings/27/dist/js/app.js
+++ landings/27/dist/js/app.js
@@ -1,26 +1,40 @@
-'use strict';
+"use strict";
 
-var contactForms = document.body.querySelectorAll('.mobile-app-guide .contact-form');
+// const contactForms = document.body.querySelectorAll('.mobile-app-guide .contact-form');
+//
+// Array.from(contactForms).forEach(form => {
+//     const thisForm = form.querySelector('form');
+//
+//     thisForm.addEventListener('submit', e => {
+//         e.preventDefault();
+//         const successMessage = form.querySelector('.success');
+//         const data = new FormData(e.target);
+//
+//         window.handleFormSubmit('https://traccoon.intellectsoft.net/forms/intellectsoft/mobile-app-guide', data, {
+//             type: 'ContactForm'
+//         }).then(response => {
+//             if (response.data.status) {
+//                 dataLayer.push({'event': 'FormSubmit'});
+//
+//                 thisForm.classList.add('hidden');
+//                 successMessage.classList.remove('hidden');
+//             }}
+//         ).catch(error => {
+//             console.error(error);
+//         })
+//     });
+// });
 
-Array.from(contactForms).forEach(function (form) {
-    var thisForm = form.querySelector('form');
+// load particles
 
-    thisForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        var successMessage = form.querySelector('.success');
-        var data = new FormData(e.target);
-
-        window.handleFormSubmit('https://traccoon.intellectsoft.net/forms/intellectsoft/mobile-app-guide', data, {
-            type: 'ContactForm'
-        }).then(function (response) {
-            if (response.data.status) {
-                dataLayer.push({ 'event': 'FormSubmit' });
-
-                thisForm.classList.add('hidden');
-                successMessage.classList.remove('hidden');
-            }
-        }).catch(function (error) {
-            console.error(error);
-        });
-    });
-});
+particlesJS("particles", { "particles": {
+        "number": {
+            "value": 80, "density": {
+                "enable": true, "value_area": 800
+            } }, "color": { "value": "#12ffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000"
+            }, "polygon": { "nb_sides": 5
+            }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 1, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 4, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.2, "width": 1.5 }, "move": { "enable": true, "speed": 1, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": false, "mode": "repulse" }, "onclick": { "enable": false, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });var count_particles, stats, _update;stats = new Stats();stats.setMode(0);stats.domElement.style.position = 'absolute';stats.domElement.style.left = '0px';stats.domElement.style.top = '0px';document.body.appendChild(stats.domElement);count_particles = document.querySelector('.js-count-particles');_update = function update() {
+    stats.begin();stats.end();if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }requestAnimationFrame(_update);
+};requestAnimationFrame(_update);;
