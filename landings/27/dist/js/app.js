05d2976593a4f509a870bc9ab3f102c17ced11e1
Serhii Vasyliev <serhii.vasyliev@root8s-MacBook-Air.local>
2019-08-27 12:01:36 +0300

    change particles js

--- landings/27/dist/js/app.js
+++ landings/27/dist/js/app.js
@@ -110,21 +110,3 @@ particlesJS("particles", {
   },
   "retina_detect": true
 });
-var count_particles, stats, _update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = 'absolute';
-stats.domElement.style.left = '0px';
-stats.domElement.style.top = '0px';
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector('.js-count-particles');
-_update = function update() {
-  stats.begin();
-  stats.end();
-  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-  }
-  requestAnimationFrame(_update);
-};
-requestAnimationFrame(_update);
-;
