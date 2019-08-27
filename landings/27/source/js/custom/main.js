54827709390de69f048a8b5fa671aa48721d94e7
Serhii Vasyliev <serhii.vasyliev@root8s-MacBook-Air.local>
2019-08-27 10:56:35 +0300

    Responsive

--- landings/27/source/js/custom/main.js
+++ landings/27/source/js/custom/main.js
@@ -1,27 +1,27 @@
-// const contactForms = document.body.querySelectorAll('.mobile-app-guide .contact-form');
-//
-// Array.from(contactForms).forEach(form => {
-//     const thisForm = form.querySelector('form');
-//
-//     thisForm.addEventListener('submit', e => {
-//         e.preventDefault();
-//         const successMessage = form.querySelector('.success');
-//         const data = new FormData(e.target);
-//
-//         window.handleFormSubmit('https://traccoon.intellectsoft.net/forms/intellectsoft/mobile-app-guide', data, {
-//             type: 'ContactForm'
-//         }).then(response => {
-//             if (response.data.status) {
-//                 dataLayer.push({'event': 'FormSubmit'});
-//
-//                 thisForm.classList.add('hidden');
-//                 successMessage.classList.remove('hidden');
-//             }}
-//         ).catch(error => {
-//             console.error(error);
-//         })
-//     });
-// });
+const contactForms = document.body.querySelectorAll('.mobile-app-guide .contact-form');
+
+Array.from(contactForms).forEach(form => {
+    const thisForm = form.querySelector('form');
+
+    thisForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const successMessage = form.querySelector('.success');
+        const data = new FormData(e.target);
+
+        window.handleFormSubmit('https://traccoon.intellectsoft.net/forms/intellectsoft/mobile-app-guide', data, {
+            type: 'ContactForm'
+        }).then(response => {
+            if (response.data.status) {
+                dataLayer.push({'event': 'FormSubmit'});
+
+                thisForm.classList.add('hidden');
+                successMessage.classList.remove('hidden');
+            }}
+        ).catch(error => {
+            console.error(error);
+        })
+    });
+});
 
 // load particles
 
